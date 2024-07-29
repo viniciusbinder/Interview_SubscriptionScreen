43e0619936347c2acdf4382a2453ad9ab20c2402
Vinícius Binder <vinicius.binder@gmail.com>
2024-07-29 12:45:37 -0300

    Add blank project

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Vinícius on 29/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
